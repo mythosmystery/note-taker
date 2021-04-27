85b52268a37b5263c28e79949fc31641f13ab497
Hunter Barton <axiysfire@gmail.com>
2021-04-27 17:40:32 -0500

    added basic server functionality

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 const express = require("express");
+const path = require("path");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+});
