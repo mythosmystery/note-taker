39055fcdfe797ecf1b0419b7d5e8f5d67284f9d3
Hunter Barton <axiysfire@gmail.com>
2021-05-01 13:49:17 -0500

    fixed bug in client side js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -96,7 +96,11 @@ const handleNoteDelete = (e) => {
 // Sets the activeNote and displays it
 const handleNoteView = (e) => {
   e.preventDefault();
-  activeNote = JSON.parse(e.target.getAttribute('data-note'));
+  if(e.target.getAttribute('data-note') == null){
+    activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
+  }else{
+    activeNote = JSON.parse(e.target.getAttribute('data-note'));
+  }  
   renderActiveNote();
 };
 
@@ -133,6 +137,7 @@ const renderNoteList = async (notes) => {
     const spanEl = document.createElement('span');
     spanEl.innerText = text;
     spanEl.style.textTransform = "capitalize";
+    spanEl.addEventListener('click', handleNoteView);
     liEl.addEventListener('click', handleNoteView);
 
     liEl.append(spanEl);
